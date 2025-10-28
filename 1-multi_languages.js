6d992f3b3508a91ace4a59c1d8168e0d9952e4ca
Michael Osei Frimpong <michael@luupli.com>
2025-10-28 20:49:19 +0000

    add multilanguage challenge

---
+++ 1-multi_languages.js
@@ -0,0 +1,7 @@
+const clangMessage = "C is fun";
+const pythonMessage = "Python is cool";
+const jsMessage = "JavaScript is amazing";
+
+console.log(clangMessage);
+console.log(pythonMessage);
+console.log(jsMessage);
